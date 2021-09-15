c6eb4d21e28b30aff1dc67517b32e2f088cd84aa
egurini <egurini@teclasystem.com>
2021-09-15 23:19:28 +0200

    Fix search after enter keyboard down

--- js/search.js
+++ js/search.js
@@ -8,4 +8,15 @@ const sendToSearchPage = () => {
 	let page = path.split("/").pop();
 	let destinationUrl =  page + "/" + storeSearchBarInputValue().replace(/\s+/g, '');
 	window.open( destinationUrl, "_self" )
+    
+}
+
+document.addEventListener('keydown', enterKeyBoardDown);
+function enterKeyBoardDown(e) {
+	if (e.keyCode === 13) {
+		//alert('Enter button clicked');
+		sendToSearchPage();
+        e.preventDefault();
+	}
+	  
 }
